a2772dfc55d541b94c359a70c381e4b30f60e246
zhengmingsheng <1438536535@qq.com>
2024-06-21 18:54:20 +0800

    index

---
+++ components/invest/invest.js
@@ -0,0 +1,69 @@
+// components/invest/invest.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    item:{
+      type:Object
+    },
+    hide:{
+      type:Boolean,
+      value:true
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    purchase:5
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    min:function(params) {
+      this.setData({
+        purchase:this.data.purchase-1
+      })
+    },
+    plus:function(params) {
+      this.setData({
+        purchase:this.data.purchase+1
+      })
+    },
+    hidepopup:function (e) {
+      if(e.target.dataset.canclose){
+        this.setData({
+        hide:true
+      })
+      }
+    },
+    purchaseconfirm:function () {
+      wx.showModal({
+        title: '提示',
+        content: "确认要购买"+this.data.purchase+"小时时长吗",
+        complete: (res) => {
+          if (res.cancel) {
+            
+          }
+      
+          if (res.confirm) {
+            App.setData({
+              time:time+=this.data.purchase
+            })
+            wx.showToast({
+              title: '购买成功',
+              duration:1500,
+              icon:'success',
+              mask:true
+            })
+          }
+        }
+      })
+    }
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,38 @@
 // index.js
-Page({})
+Page({
+  data:{
+    item:{},
+    hidepopup:true
+  },
+  LoadSeat:function showloading() {
+    wx.showLoading({
+      title: '加载座位信息',
+    })
+    setTimeout(function () {
+       wx.navigateTo({
+      url: '/pages/reserve/reserve',
+    })
+      wx.hideLoading()
+    },1500)
+   
+  },
+  LeaveSeat:function showloading() {
+    wx.showLoading({
+      title: '释放座位资源',
+    })
+    setTimeout(function () {
+      wx.showToast({
+        title: '释放成功',
+        duration:1000,
+        icon:'success',
+        mask:true
+      })
+     wx.hideLoading()
+   },1500)
+  },
+  onshowpopup:function() {
+    this.setData({
+      hidepopup:false
+    })
+  }
+})
